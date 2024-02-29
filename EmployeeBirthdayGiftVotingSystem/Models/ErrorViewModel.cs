cec062899c4d0fe274c5849f433efb6f2f878f9f
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-02-29 18:58:01 +0200

    Add project files.

---
+++ EmployeeBirthdayGiftVotingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeBirthdayGiftVotingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
