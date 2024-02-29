3435a017eb0f11cebd2b93abb90ba03eb0f565aa
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2024-02-29 19:46:00 +0200

    Initialize workflows and unit test project (#2)
    * create test project
    
    * add NSubstitute
    
    * create workflows
    
    * specify project

---
+++ EmployeeBirthdayGiftVotingSystem.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EmployeeBirthdayGiftVotingSystem.Tests.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
