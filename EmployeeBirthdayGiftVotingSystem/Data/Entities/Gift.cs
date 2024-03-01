4a994c6641b60b8acba697f189666f193d7cffd1
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-03-01 14:45:26 +0200

    create Gift entity

---
+++ EmployeeBirthdayGiftVotingSystem/Data/Entities/Gift.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeBirthdayGiftVotingSystem.Data.Entities
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Examples: "toothbrush.jpg", "black_dice.png", etc.
+        /// </summary>
+        [Required]
+        public string ImageFileName { get; set; } = string.Empty;
+    }
+}
