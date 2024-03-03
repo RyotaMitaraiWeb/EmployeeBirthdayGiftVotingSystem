d0bace005ba24b4835cdc5eecf5832001bbaeeda
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-03-03 21:06:34 +0200

    commit view models

---
+++ EmployeeBirthdayGiftVotingSystem/Models/Vote/CreateVoteListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeBirthdayGiftVotingSystem.Models.Vote
+{
+    public class CreateVoteListViewModel
+    {
+        public string EmployeeId { get; set; } = string.Empty;
+        public IEnumerable<CreateVoteViewModel> AvailableUsers { get; set; } = [];
+    }
+}
---
+++ EmployeeBirthdayGiftVotingSystem/Models/Vote/CreateVoteViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeBirthdayGiftVotingSystem.Models.Vote
+{
+    public class CreateVoteViewModel
+    {
+        public string EmployeeId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
