7603d2e8ef2747bf51c660a19d2fcc3218a2c91e
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-03-04 03:17:24 +0200

    create a component for the vote index

---
+++ EmployeeBirthdayGiftVotingSystem/Views/Shared/Components/VoteCard/VoteCardViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeBirthdayGiftVotingSystem.Models.Vote;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeBirthdayGiftVotingSystem.Views.Shared.Components.VoteCard
+{
+    public class VoteCardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(VoteIndexViewModel vote)
+        {
+            return vote.IsActive ? View("ActiveVoteCard", vote) : View("InactiveVoteCard", vote);
+        }
+    }
+}
